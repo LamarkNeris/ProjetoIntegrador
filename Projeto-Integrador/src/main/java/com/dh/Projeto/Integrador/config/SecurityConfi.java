f2e53292eb7d6f5a7fc4ae9b28643332f710f01f
Lamark <lamark12.neris@gmail.com>
2022-03-31 22:34:40 -0300

    sprint03

--- Projeto-Integrador/src/main/java/com/dh/Projeto/Integrador/config/SecurityConfi.java
+++ Projeto-Integrador/src/main/java/com/dh/Projeto/Integrador/config/SecurityConfi.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,17 +18,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfi extends WebSecurityConfigurerAdapter {
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/usuario/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/usuario/").permitAll()
                 .antMatchers(HttpMethod.GET,"/produto/","/categoria/","/cidade/","/usuario/").permitAll()
                 .antMatchers(HttpMethod.PUT,"/produto/","/categoria/","/cidade/","/usuario/").permitAll()
                 .antMatchers(HttpMethod.DELETE,"/produto/","/categoria/","/cidade/","/usuario/").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .httpBasic();
 
--- Projeto-Integrador/src/main/java/com/dh/Projeto/Integrador/controller/CategoriaController.java
+++ Projeto-Integrador/src/main/java/com/dh/Projeto/Integrador/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import com.dh.Projeto.Integrador.service.CategoriaService;
 import com.dh.Projeto.Integrador.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +35,7 @@ public class CategoriaController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> remove(@PathVariable Integer id){
         categoriaService.delete(id);
         return ResponseEntity.ok("Categoria excluída!!");
